c9d95e7ab9be05156d7fcc66084c2226e2660ef6
Renee <lrenee.mo@sbcglobal.net>
2020-03-11 11:03:29 -0500

    initial build

---
+++ src/main/java/org/divvybuy/divvybuy/DivvyBuyApplication.java
@@ -0,0 +1,13 @@
+package org.divvybuy.divvybuy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DivvyBuyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DivvyBuyApplication.class, args);
+	}
+
+}
